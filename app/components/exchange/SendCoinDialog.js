f9236ac988219454e0d2e029f6a7c33432e4edb1
Eric Juta <ericjohn.juta@consensys.net>
2018-08-09 23:03:06 +0100

    Add login dialog prompt for sending and receiving cryptocurrency

--- app/components/exchange/SendCoinDialog.js
+++ app/components/exchange/SendCoinDialog.js
@@ -17,174 +17,184 @@ import { formattedAmountToFloat, formattedAmountToBigNumber } from '../../utils/
 import { DECIMAL_PLACES_IN_LUX, MAX_INTEGER_PLACES_IN_LUX } from '../../config/numbersConfig';
 
 export const messages = defineMessages({
-    
-    receiverLabel: {
-      id: 'wallet.send.form.receiver.label',
-      defaultMessage: '!!!Receiver',
-      description: 'Label for the "Receiver" text input in the wallet send form.'
-    },
-    receiverHint: {
-      id: 'wallet.send.form.receiver.hint',
-      defaultMessage: '!!!Wallet Address',
-      description: 'Hint inside the "Receiver" text input in the wallet send form.'
-    },
-    amountLabel: {
-      id: 'wallet.send.form.amount.label',
-      defaultMessage: '!!!Amount',
-      description: 'Label for the "Amount" number input in the wallet send form.'
-    },
-    invalidAmount: {
-      id: 'wallet.send.form.errors.invalidAmount',
-      defaultMessage: '!!!Please enter a valid amount.',
-      description: 'Error message shown when invalid amount was entered.',
-    }
+  receiverLabel: {
+    id: 'wallet.send.form.receiver.label',
+    defaultMessage: '!!!Receiver',
+    description: 'Label for the "Receiver" text input in the wallet send form.'
+  },
+  receiverHint: {
+    id: 'wallet.send.form.receiver.hint',
+    defaultMessage: '!!!Wallet Address',
+    description: 'Hint inside the "Receiver" text input in the wallet send form.'
+  },
+  amountLabel: {
+    id: 'wallet.send.form.amount.label',
+    defaultMessage: '!!!Amount',
+    description: 'Label for the "Amount" number input in the wallet send form.'
+  },
+  invalidAmount: {
+    id: 'wallet.send.form.errors.invalidAmount',
+    defaultMessage: '!!!Please enter a valid amount.',
+    description: 'Error message shown when invalid amount was entered.'
+  }
 });
 
 messages.fieldIsRequired = globalMessages.fieldIsRequired;
 
 type Props = {
-    currencyMaxIntegerDigits?: number,
-    currencyMaxFractionalDigits: number,
-    coinName: string,
-    balance: string,
-    isSubmitting: boolean,
-    error: ?LocalizableError,
-    onCancel: Function
+  currencyMaxIntegerDigits?: number,
+  currencyMaxFractionalDigits: number,
+  coinName: string,
+  balance: string,
+  isSubmitting: boolean,
+  error: ?LocalizableError,
+  onCancel: Function,
+  isLogined: boolean
 };
 
 @observer
-export default class ReceiveAddressDialog extends Component<Props> {
+export default class SendCoinDialog extends Component<Props> {
+  static contextTypes = {
+    intl: intlShape.isRequired
+  };
 
-    static contextTypes = {
-        intl: intlShape.isRequired,
-    };
+  // FORM VALIDATION
+  form = new ReactToolboxMobxForm(
+    {
+      fields: {
+        receiver: {
+          label: 'Receiver',
+          placeholder: this.context.intl.formatMessage(messages.receiverHint),
+          value: '',
+          validators: [
+            ({ field }) => {
+              if (field.value === '') {
+                return [false, this.context.intl.formatMessage(messages.fieldIsRequired)];
+              }
 
-    // FORM VALIDATION
-    form = new ReactToolboxMobxForm({
-        fields: {
-            receiver: {
-            label: 'Receiver',
-            placeholder: this.context.intl.formatMessage(messages.receiverHint),
-            value: '',
-            validators: [({ field }) => {
-                if (field.value === '') {
-                    return [false, this.context.intl.formatMessage(messages.fieldIsRequired)];
-                }
-                
-                return [true];
-            }],
-            },
-            amount: {
-            label: this.context.intl.formatMessage(messages.amountLabel),
-            placeholder: `0.${'0'.repeat(this.props.currencyMaxFractionalDigits)}`,
-            value: '',
-            validators: [async ({ field }) => {
-                if (field.value === '') {
-                    return [false, this.context.intl.formatMessage(messages.fieldIsRequired)];
-                }
-                else if(formattedAmountToFloat(field.value) > parseFloat(this.props.balance))
-                {
-                    return [false, this.context.intl.formatMessage(messages.invalidAmount)];
-                }
-                else
-                    return [true, this.context.intl.formatMessage(messages.invalidAmount)];
-            }],
-            },
+              return [true];
+            }
+          ]
         },
-        }, {
-        options: {
-            validateOnBlur: false,
-            validateOnChange: true,
-            validationDebounceWait: 250,
-        },
+        amount: {
+          label: this.context.intl.formatMessage(messages.amountLabel),
+          placeholder: `0.${'0'.repeat(this.props.currencyMaxFractionalDigits)}`,
+          value: '',
+          validators: [
+            async ({ field }) => {
+              if (field.value === '') {
+                return [false, this.context.intl.formatMessage(messages.fieldIsRequired)];
+              } else if (formattedAmountToFloat(field.value) > parseFloat(this.props.balance)) {
+                return [false, this.context.intl.formatMessage(messages.invalidAmount)];
+              }
+              return [true, this.context.intl.formatMessage(messages.invalidAmount)];
+            }
+          ]
+        }
+      }
+    },
+    {
+      options: {
+        validateOnBlur: false,
+        validateOnChange: true,
+        validationDebounceWait: 250
+      }
+    }
+  );
+
+  submit() {
+    this.form.submit({
+      onSuccess: form => {
+        const { coinName } = this.props;
+        const { receiver, amount } = form.values();
+        const transactionData = {
+          coin: coinName,
+          receiver,
+          amount: formattedAmountToFloat(amount)
+        };
+        this.props.onSubmit(transactionData);
+      },
+      onError: () => {}
     });
+  }
 
-    submit() {
-        this.form.submit({
-          onSuccess: (form) => {
-            const { coinName } = this.props;
-            const { receiver, amount } = form.values();
-            const transactionData = {
-                coin: coinName,
-                receiver,
-                amount: formattedAmountToFloat(amount),
-            };
-            this.props.onSubmit(transactionData);
-          },
-          onError: () => {}
-        });
-    }
+  render() {
+    const { form } = this;
+    const { intl } = this.context;
 
-    render() {
-        const { form } = this;
-        const { intl } = this.context;
-        
-        const {
-            currencyMaxIntegerDigits, 
-            currencyMaxFractionalDigits,
-            coinName,
-            error,
-            isSubmitting,
-            onCancel
-        } = this.props;
+    const {
+      currencyMaxIntegerDigits,
+      currencyMaxFractionalDigits,
+      coinName,
+      error,
+      isSubmitting,
+      onCancel,
+      isLogined
+    } = this.props;
 
-        const amountField = form.$('amount');
-        const receiverField = form.$('receiver');
-        const amountFieldProps = amountField.bind();
-        const totalAmount = formattedAmountToBigNumber(amountFieldProps.value);
+    const amountField = form.$('amount');
+    const receiverField = form.$('receiver');
+    const amountFieldProps = amountField.bind();
+    const totalAmount = formattedAmountToBigNumber(amountFieldProps.value);
 
-        const confirmButtonClasses = classnames([
-            'confirmButton',
-            isSubmitting ? styles.submitButtonSpinning : null,
-        ]);
-      
-        const actions = [
-            {
-              label: 'Cancel',
-              onClick: !isSubmitting && onCancel,
-            },
-            {
-              label: 'Send',
-              onClick: this.submit.bind(this),
-              primary: true,
-              className: confirmButtonClasses,
-              disabled: !amountField.isValid || !receiverField.isValid
-            },
-         ];
+    const confirmButtonClasses = classnames([
+      'confirmButton',
+      isSubmitting ? styles.submitButtonSpinning : null
+    ]);
 
-        return (
-            <Dialog
-                title={coinName}
-                actions={actions}
-                closeOnOverlayClick
-                onClose={onCancel}
-                className={styles.dialog}
-                closeButton={<DialogCloseButton onClose={onCancel} />}
-              >
-                <div className={styles.receiverInput}>
-                    <Input
-                    className="receiver"
-                    {...receiverField.bind()}
-                    error={receiverField.error}
-                    skin={<SimpleInputSkin />}
-                    />
-                </div>
+    const actions = [
+      {
+        label: 'Cancel',
+        onClick: !isSubmitting && onCancel
+      },
+      {
+        label: 'Send',
+        onClick: this.submit.bind(this),
+        primary: true,
+        className: confirmButtonClasses,
+        disabled: !amountField.isValid || !receiverField.isValid
+      }
+    ];
 
-                <div className={styles.amountInput}>
-                    <Input
-                    {...amountFieldProps}
-                    className="amount"
-                    label="Amount"
-                    error={amountField.error}
-                    // AmountInputSkin props
-                    currency={coinName}
-                    total={totalAmount.toFormat(currencyMaxFractionalDigits)}
-                    skin={<AmountInputSkin />}
-                    />
-                </div>
-                {error ? <p className={styles.error}>{intl.formatMessage(error)}</p> : null}
-            </Dialog>
-        );
-    }
+    return (
+      <Dialog
+        title={coinName}
+        actions={actions}
+        closeOnOverlayClick
+        onClose={onCancel}
+        className={styles.dialog}
+        closeButton={<DialogCloseButton onClose={onCancel} />}
+      >
+        {isLogined ? (
+          <div>
+            <div className={styles.receiverInput}>
+              <Input
+                className="receiver"
+                {...receiverField.bind()}
+                error={receiverField.error}
+                skin={<SimpleInputSkin />}
+              />
+            </div>
 
+            <div className={styles.amountInput}>
+              <Input
+                {...amountFieldProps}
+                className="amount"
+                label="Amount"
+                error={amountField.error}
+                // AmountInputSkin props
+                currency={coinName}
+                total={totalAmount.toFormat(currencyMaxFractionalDigits)}
+                skin={<AmountInputSkin />}
+              />
+            </div>
+          </div>
+        ) : (
+          <div className={styles.info}>Please login first to send your cryptocurrency!</div>
+        )}
+
+        {error ? <p className={styles.error}>{intl.formatMessage(error)}</p> : null}
+      </Dialog>
+    );
+  }
 }
