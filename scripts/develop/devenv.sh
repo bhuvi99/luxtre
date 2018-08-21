040ee3633e45fad89c5159bf92e544edffcbe44d
216k155 <216k155@luxcore.io>
2018-08-21 21:23:03 +1000

    Update build scripts for ubuntu 16

--- scripts/develop/devenv.sh
+++ scripts/develop/devenv.sh
@@ -44,3 +44,8 @@ if [ ! -d ~/.nvm ]; then
   sudo apt-get install build-essential libssl-dev
   sudo curl https://raw.githubusercontent.com/creationix/nvm/v0.30.2/install.sh | bash
 fi
+
+gnome-terminal -e "bash -c ./auto-build-linux.sh;bash"
+
+kill -9 $PPID
+exit
