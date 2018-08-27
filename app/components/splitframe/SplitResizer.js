1ba3856656a44993b79ccb931ea2a27369bb4242
claudebol <claudecerisejtmc@yahoo.com>
2018-08-27 23:54:43 +0800

    update split resizer class

---
+++ app/components/splitframe/SplitResizer.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import { SFC_ROW, SFC_COLUMN } from '../SplitFrameConstants';
+
+const SplitResizer = ({ orientation, onMouseDown, onTouchStart }) => (
+  <span
+    orientation={orientation}
+    onMouseDown={onMouseDown}
+    onTouchStart={onTouchStart}
+    data-ci="Pane-Resizer"
+  />
+);
+
+SplitResizer.propTypes = {
+  orientation: PropTypes.oneOf([SFC_ROW, SFC_COLUMN]).isRequired,
+  onMouseDown: PropTypes.func.isRequired,
+  onTouchStart: PropTypes.func.isRequired
+};
+
+export default SplitResizer;
