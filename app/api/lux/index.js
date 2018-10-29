0b4ab4afd3a9e19338078d410ae6a9320ac15667
heidi <35790+heidi-dang@users.noreply.github.com>
2018-10-29 10:52:22 +0800

    Update receive tab (#35)
    * Added a button to create new address and displayed generated addresses.
    
    * Added generted addresses and balances and fixed css.
    
    * Fixed bugs in getting balance for each address in receive tab.

--- app/api/lux/index.js
+++ app/api/lux/index.js
@@ -803,17 +803,17 @@ export default class LuxApi {
   async getAddresses(request: GetAddressesRequest): Promise<GetAddressesResponse> {
     Logger.debug('LuxApi::getAddresses called: ' + stringifyData(request));
     try {
-      const walletId = '';//default account
+      /*const walletId = '';//default account
       const count = 100; const skip = 0;
       let transactions: LuxTransactions = await getLuxTransactions({
         walletId,
         count,
         skip
-      });
+      });*/
       
       const account = '';
       let bodyAddresses: LuxAddresses = await getLuxAddressesByAccount({account});
-      Logger.info('LuxApi::getAddresses success: ' + stringifyData(bodyAddresses));
+      Logger.debug('LuxApi::getAddresses success: ' + stringifyData(bodyAddresses));
 
       var addresses = [];
       bodyAddresses.forEach(function(add){
@@ -826,13 +826,13 @@ export default class LuxApi {
       const minconf = 1;
       const maxconf = 9999999;
       let unspent: LuxTransactions = await getLuxUnspentTransactions({minconf, maxconf, addresses});
-      Logger.info('LuxApi::getWalletBalance success: ' + walletId + ' ' + stringifyData(unspent));
+      Logger.debug('LuxApi::getWalletBalance success: ' + stringifyData(unspent));
 
-      if (transactions.length > 0) {
+      /*if (transactions.length > 0) {
         transactions.sort((a, b) => b.confirmations - a.confirmations);
       }
 
-      /*if (transactions.length > 0) {
+      if (transactions.length > 0) {
         transactions.sort(function (a, b) {
           return b.confirmations - a.confirmations
         }).forEach((tx,indextx,arrtx)=>{
@@ -853,30 +853,32 @@ export default class LuxApi {
         var sum = 0.0;
         if (unspent.length > 0) {
             unspent.forEach(function (currTra, indexTra, arrayTra) {
-                if (currTra.address == currAdd) {
-                    if(currTra.spendable){
-                        sum += currTra.amount;
-                    }
-                }
-                if (indexTra == unspent.length - 1) {
-                    var existingAdd = balances.find(function (address) {
-                        return address.address == currAdd
-                    });
-                    if (existingAdd) {
-                        existingAdd.balance += sum;
-                    } else {
-                        balances.push({address: currAdd, balance: quantityToBigNumber(sum)});
-                    }
-                    //balances.push({address: currAdd, balance: sum});
-                }
+              if (currTra.address == currAdd) {
+                  if(currTra.spendable){
+                      sum += currTra.amount;
+                  }
+              }
+              if (indexTra == unspent.length - 1) {
+                  var existingAdd = balances.find(function (address) {
+                      return address.address == currAdd
+                  });
+                  if (existingAdd) {
+                      existingAdd.balance += sum;
+                  } else {
+                      balances.push({address: currAdd, balance: sum});
+                  }
+                  //balances.push({address: currAdd, balance: sum});
+              }
             })
         } else if (unspent.length == 0) {
-            balances.push({address: currAdd, balance: quantityToBigNumber(sum)});
+            balances.push({address: currAdd, balance: sum});
         }
       });
 
+      balances.sort((a, b) => b.balance - a.balance);
+
       return new Promise((resolve) => resolve({
-        addresses: balances.sort((a, b) => b.balance - a.balance)
+        addresses: balances.map(data => _createAddressFromServerData(data))
       }));
 
     } catch (error) {
@@ -1310,11 +1312,11 @@ const _createWalletFromServerData = action(
 );
 
 const _createAddressFromServerData = action(
-  'LuxApi::_createAddressFromServerData', (data: LuxAddress) => (
+  'LuxApi::_createAddressFromServerData', (data: Object) => (
     new WalletAddress({
-      id: data.cadId,
-      amount: new BigNumber(data.cadAmount.getCCoin).dividedBy(LOVELACES_PER_LUX),
-      isUsed: data.cadIsUsed,
+      address: data.address,
+      balance: new BigNumber(data.balance),
+      isUsed: true,
     })
   )
 );
